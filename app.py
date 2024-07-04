decded5e25cc99fb7c6fb5049fbd984bfcbf269b
shanskarBansal <115683341+shanskarBansal@users.noreply.github.com>
2024-07-04 19:37:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,13 +201,13 @@ def app_second_block():
     client = gspread.authorize(creds)
 
 
-    Google_api_credential_file = (st.secrets["script_service_account"])
+    Google_api_credential_file = dict(st.secrets["script_service_account"])
     with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp_file:
         temp_file.write(json.dumps(Google_api_credential_file).encode())
-        temp_file_path = temp_file.name
-
-    creddd =  ServiceAccountCredentials.from_json_keyfile_dict(
-        temp_file.name,
+        
+        
+    creddd = service_account.Credentials.from_service_account_info(
+        temp_file,
         scoped = [
         'https://www.googleapis.com/auth/script.projects',
         'https://www.googleapis.com/auth/drive',
